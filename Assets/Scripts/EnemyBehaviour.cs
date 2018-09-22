7913fd5e31aec3ca76f90115b6e135765b9ab764
Lui Rocha <luifr@usp.br>
2018-09-21 23:35:42 -0300

    enemy dies

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour {
 
 	[SerializeField] private float speed = 1.1f;
+	[SerializeField] private float hp = 100f;
+	[SerializeField] private float dano = 20f;
 	private Vector3 movement;
 
 	// Use this for initialization
@@ -14,8 +16,26 @@ public class EnemyBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		movement = transform.position;
 		movement.x-=speed*Time.deltaTime;
-		transform.position=movement;
+		transform.position=movement;	
+
+		if(hp <= 0){
+			Destroy(gameObject);
+		}
+
 	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		Debug.Log("hit");
+		if(collision.gameObject.name.StartsWith("SkeletonAttack")){
+			hp-=dano;
+			Debug.Log("dano");
+		}
+		// else if(collision.gameObject.tag == "minion" ){
+
+		// }
+	}
+
 }
