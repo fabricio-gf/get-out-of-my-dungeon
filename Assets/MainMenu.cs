97e698bab71795f44fc0b0194d11a389115d6fc1
Fabricio <fabricio-gf@hotmail.com>
2018-09-23 02:26:27 -0300

    Added menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject menu;
+	public GameObject credits;
+
+	public void PlayGame(){
+		SceneManager.LoadScene("rodrigo");
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+
+	public void ToggleCredits(){
+		menu.SetActive(!menu.activeSelf);
+		credits.SetActive(!credits.activeSelf);
+	}
+}
