8c48ae8edfdc4ab112752b545b43a3253f2a5d2d
Lui Rocha <luifr@usp.br>
2018-09-23 13:06:19 -0300

    movimentacao

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class EnemyBehaviour : MonoBehaviour
 {
@@ -10,14 +11,16 @@ public class EnemyBehaviour : MonoBehaviour
     [SerializeField] private float hp = 100f;
     [SerializeField] private float dano = 20f;
     [SerializeField] private GameObject PopupTextPrefab;
+    private Tilemap tilemap;
     private GameObject TextObject;
+    private int dir=0;
     private GameObject Canvas;
     private Text TextScript;
     private Vector3 movement;
     private bool col = false;
     private float stopTime = 1, stopTimer;
     public float damagetimer = 0, immuneDamageTime = 1.6f;
-
+    private TileBase tilecell;
     public void CreateFloatingText(string text, Transform location)
     {
         GameObject instance = Instantiate(PopupTextPrefab);
@@ -32,6 +35,7 @@ public class EnemyBehaviour : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        tilemap = GameObject.Find("/Grid/Tilemap").GetComponent<Tilemap>();
         Canvas = GameObject.Find("/Canvas");
         TextObject = GameObject.Find("/Canvas/SideBar/Money/Text");
         TextScript = TextObject.GetComponent<Text>();
@@ -40,20 +44,47 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!col && stopTimer <= 0)
-        {
-            movement = transform.position;
-            movement.x -= speed * Time.deltaTime;
-            transform.position = movement;
+        if(!gameObject.name.StartsWith("Angel") ){   
+            Vector3Int v = Vector3Int.FloorToInt(gameObject.transform.position);
+            v.x-=1;
+            v.z = 0;
+            tilecell = tilemap.GetTile(v);
+            if(tilecell && tilecell.name.StartsWith("Floor(1)_0")){
+                //gameover
+            }
+            if(tilecell && tilecell.name.StartsWith("Wall")){
+                if(dir==0){
+                    v = Vector3Int.RoundToInt(gameObject.transform.position);
+                    v.z=0;
+                    int x;
+                    if( (x=Mathf.RoundToInt( Random.Range(0,1) )) == 0)
+                        v.y-=1;
+                    else
+                        v.y+=1;
+                    tilecell = tilemap.GetTile(v);
+                    if(tilecell && tilecell.name.StartsWith("Wall")){
+                        if(x==0)
+                            dir = 2; // desce
+                        else
+                            dir=1;
+                    }
+                    else{
+                        if(x==0)
+                            dir = 1; // desce
+                        else
+                            dir=2;
+                    }
+                }
+            }
+            else{
+                if(dir!=0)
+                    move();
+                    move();
+                dir = 0; // em frente
+            }
         }
-        else if (col)
-        {
-            movement = transform.position;
-            movement.x += 6 * speed * Time.deltaTime;
-            transform.position = movement;
-            stopTimer = stopTime;
-            col = false;
-        }
+        move();
+
 
         damagetimer -= Time.deltaTime;
         stopTimer -= Time.deltaTime;
@@ -67,8 +98,40 @@ public class EnemyBehaviour : MonoBehaviour
         }
     }
 
+    public void move(){
+
+        if (!col && stopTimer <= 0)
+        {
+            movement = transform.position;
+            
+            if(dir==0)
+                movement.x -= speed * Time.deltaTime;
+            else if(dir==1)
+                movement.y -= speed * Time.deltaTime;
+            else if(dir==2)
+                movement.y += speed * Time.deltaTime;
+            
+            transform.position = movement;
+        }
+        else if (col)
+        {
+            movement = transform.position;
+            if(dir==0)
+                movement.x += 6 * speed * Time.deltaTime;
+            else if(dir==1)
+                movement.x += 6 * speed * Time.deltaTime;
+            else if(dir==2)
+                movement.x += 6 * speed * Time.deltaTime;
+            transform.position = movement;
+            stopTimer = stopTime;
+            col = false;
+        }
+
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("oi");
         if (collision.gameObject.name.StartsWith("SkeletonAttack"))
         {
             hp -= dano;
@@ -78,6 +141,9 @@ public class EnemyBehaviour : MonoBehaviour
         {
             // nothing
         }
+        // else if(){
+
+        // }
         else
         {
             Minion m = collision.gameObject.GetComponent<Minion>();
